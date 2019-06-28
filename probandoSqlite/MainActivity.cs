09744e491ab59890490fb4374de5b8f94724f6bb
danielsolari451 <danielsolari451@hotmail.com>
2019-06-28 16:00:21 -0300

    First

---
+++ probandoSqlite/MainActivity.cs
@@ -0,0 +1,119 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using probandoSqlite.Resources.Model;
+using System.Collections.Generic;
+using probandoSqlite.Resources.DataHelper;
+using probandoSqlite.Resources;
+
+namespace probandoSqlite
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        ListView lstData;
+        List<Person> lstSource = new List<Person>();
+        DataBase db;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //Create DataBase
+            db = new DataBase();
+            db.createDataBase();
+
+            lstData = FindViewById<ListView>(Resource.Id.listView);
+
+            var edtName = FindViewById<EditText>(Resource.Id.edtName);
+            var edtAge = FindViewById<EditText>(Resource.Id.edtAge);
+            var edtEmail = FindViewById<EditText>(Resource.Id.edtEmail);
+
+            var btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            var btnEdit = FindViewById<Button>(Resource.Id.btnEdit);
+            var btnDelete = FindViewById<Button>(Resource.Id.btnDelete);
+            
+            //LoadData
+            LoadData();
+
+            //Event
+            btnAdd.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+
+                db.InsertIntoTablePerson(person);
+                LoadData();
+            };
+
+            btnEdit.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+
+                db.updateTablePerson(person);
+                LoadData();
+            };
+
+            btnDelete.Click += delegate
+            {
+                Person person = new Person()
+                {
+                    Id = int.Parse(edtName.Tag.ToString()),
+                    Name = edtName.Text,
+                    Age = int.Parse(edtAge.Text),
+                    Email = edtEmail.Text
+                };
+
+                db.deleteTablePerson(person);
+                LoadData();
+            };
+
+
+            lstData.ItemClick += (s,e) => {
+                for (int i = 0; i < lstData.Count; i++)
+                {
+                    if (e.Position == i) {
+
+                        lstData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.DarkGray);
+
+                    }
+                    else
+                        lstData.GetChildAt(i).SetBackgroundColor(Android.Graphics.Color.Transparent);
+                }
+
+                var txtName = e.View.FindViewById<TextView>(Resource.Id.textView1);
+                var txtAge = e.View.FindViewById<TextView>(Resource.Id.textView2);
+                var txtEmail = e.View.FindViewById<TextView>(Resource.Id.textView3);
+
+                edtName.Text = txtName.Text;
+                edtName.Tag = e.Id;
+                edtAge.Text = txtName.Text;
+
+                edtEmail.Text = txtEmail.Text;
+
+            };
+        }
+
+        private void LoadData() {
+
+            lstSource = db.selectTablePerson();
+            var adapter = new ListViewAdapter(this, lstSource);
+            lstData.Adapter = adapter;
+        }
+    }
+}
---
+++ probandoSqlite/Resources/DataHelper/DataBase.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using probandoSqlite.Resources.Model;
+using SQLite;
+
+namespace probandoSqlite.Resources.DataHelper
+{
+    public class DataBase
+    {
+        string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+
+        public bool createDataBase() {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db"))) {
+
+                    connection.CreateTable<Model.Person>();
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool InsertIntoTablePerson(Person person)
+        {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+
+                    connection.Insert(person);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public List<Person> selectTablePerson()
+        {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+
+                    return connection.Table<Person>().ToList();
+                    
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+
+        public bool updateTablePerson(Person person)
+        {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+
+                    connection.Query<Person>("UPDATE Person set Name=?,Age=?,Email=? Where Id=?",person.Name,person.Age,person.Email,person.Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool deleteTablePerson(Person person)
+        {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+                    //var pr = connection.Table<Person>().First(x => x.Id == person.Id);
+                    connection.Delete(person);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+        public bool selectQueryTablePerson(int Id)
+        {
+
+            try
+            {
+                using (var connection = new SQLiteConnection(System.IO.Path.Combine(folder, "Persons.db")))
+                {
+
+                    connection.Query<Person>("SELECT * FROM Person Where Id=?", Id);
+                    return true;
+                }
+            }
+            catch (SQLiteException ex)
+            {
+
+                Log.Info("SQLiteEx", ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
